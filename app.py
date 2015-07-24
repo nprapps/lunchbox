94cac876df4ffb53cfbc16654622affea8aaf40f
Alyson Hurt <ahurt@npr.org>
2015-07-24 11:00:25 -0400

    navigation. closes #25

--- app.py
+++ app.py
@@ -29,24 +29,28 @@ def index():
     """
     context = make_context()
     context['name'] = 'TKTK App Name'
+    context['id'] = 'home'
     return make_response(render_template('index.html', **context))
 
 @app.route('/factlist/index.html')
 def factlist():
     context = make_context()
     context['name'] = 'Factlist'
+    context['id'] = context['name']
     return make_response(render_template('factlist.html', **context))
 
 @app.route('/quotable/index.html')
 def quotable():
     context = make_context()
     context['name'] = 'Quotable'
+    context['id'] = context['name']
     return make_response(render_template('quotable.html', **context))
 
 @app.route('/waterbug/index.html')
 def waterbug():
     context = make_context()
     context['name'] = 'Waterbug'
+    context['id'] = context['name']
     return make_response(render_template('waterbug.html', **context))
 
 app.register_blueprint(static.static)
