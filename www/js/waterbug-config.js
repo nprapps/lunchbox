cc7e659c0335c9e77b72f28ffdd48c496b95f2b4
Tyler Fisher <tyler@tylerjfisher.com>
2015-07-27 13:16:22 -0400

    add NPR-specific config
    Former-commit-id: d8f88e6ae65b84b1ab59b325b2ad996de0bbb042

--- www/js/waterbug-config.js
+++ www/js/waterbug-config.js
@@ -5,19 +5,19 @@ var elementPadding = 40; // padding around the logo and credit text
 // logo configuration
 // the name of the logo object should match the value of the corresponding radio button in the HTML.
 var logos = {
-    'lunchbox': {
-        whitePath: '../img/icon-lunchbox-white.svg', // path to white logo
-        blackPath: '../img/icon-lunchbox-black.svg', // path to black logo
-        w: 100, // width of logo
-        h: 80, // height of logo
-        display: 'Lunchbox'
+    'npr': {
+        whitePath: '../img/logo-npr-white.svg', // path to white logo
+        blackPath: '../img/logo-npr-black.svg', // path to black logo
+        w: 150, // width of logo
+        h: 51, // height of logo
+        display: 'NPR'
     },
-    'socializr': {
-        whitePath: '../img/icon-socializr-white.svg',
-        blackPath: '../img/icon-socializr-black.svg',
-        w: 150,
-        h: 51,
-        display: 'Socializr'
+    'music': {
+        whitePath: '../img/logo-music-white.svg',
+        blackPath: '../img/logo-music-black.svg',
+        w: 306,
+        h: 81,
+        display: 'NPR Music'
     }
 };
 
@@ -28,14 +28,14 @@ var blackLogoAlpha = '0.6';
 // type
 var fontWeight = 'normal'; // font weight for credit
 var fontSize = '20pt'; // font size for credit
-var fontFace = "Helvetica"; // font family for credit
+var fontFace = "Gotham Ssm"; // font family for credit
 var fontShadow = 'rgba(0,0,0,0.7)'; // font shadow for credit
 var fontShadowOffsetX = 0; // font shadow offset x
 var fontShadowOffsetY = 0; // font shadow offset y
 var fontShadowBlur = 10; // font shadow blur
 
 // copyright options
-var orgName = 'Your News Organization';
+var orgName = 'NPR';
 var freelanceString = 'for ' + orgName;
 
 var copyrightOptions = {
@@ -45,7 +45,7 @@ var copyrightOptions = {
         photographerRequired: false, // require a photographer
         sourceRequired: false, // require a source
         source: orgName, // How the source should appear on the image, e.g. 'NPR'
-        display: orgName, // How the option will appear in the dropdown menu   
+        display: orgName, // How the option will appear in the dropdown menu
     },
     'freelance': {
         showPhotographer: true,
@@ -53,7 +53,7 @@ var copyrightOptions = {
         photographerRequired: true,
         sourceRequired: false,
         source: freelanceString,
-        display: 'Freelance' 
+        display: 'Freelance'
     },
     'ap': {
         showPhotographer: true,
@@ -61,7 +61,7 @@ var copyrightOptions = {
         photographerRequired: false,
         sourceRequired: false,
         source: 'AP',
-        display: 'AP' 
+        display: 'AP'
     },
     'getty': {
         showPhotographer: true,
@@ -69,7 +69,7 @@ var copyrightOptions = {
         photographerRequired: false,
         sourceRequired: false,
         source: 'Getty Images',
-        display: 'Getty' 
+        display: 'Getty'
     },
     'thirdParty': {
         showPhotographer: true,
@@ -77,13 +77,13 @@ var copyrightOptions = {
         photographerRequired: false,
         sourceRequired: true,
         source: '',
-        display: 'Third Party/Courtesy' 
+        display: 'Third Party/Courtesy'
     }
 }
 
 // app load defaults
 var currentCrop = 'twitter'; // default crop size
-var currentLogo = 'lunchbox'; // default logo slug
+var currentLogo = 'npr'; // default logo slug
 var currentLogoColor = 'white'; // default logo color
 var currentTextColor = 'white'; // default text color
 var defaultImage = '../img/test-kitten.jpg'; // path to image to load as test image
