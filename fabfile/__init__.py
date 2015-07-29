fbaffb5c0b835fe727691efbc3a6a7ccb4c0c2bd
Tyler Fisher <tyler@tylerjfisher.com>
2015-07-29 11:39:42 -0400

    port a fix to npr branch
    Former-commit-id: 861295b1f80c36cbadcc88295ab15f0d3683a51f

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -154,7 +154,7 @@ def deploy(remote='origin', reload=False):
             app_config.PROJECT_SLUG
         ))
 
-    else:
+    if env.settings == 'production' or env.settings == 'master':
         flat.deploy_folder(
             app_config.S3_BUCKET,
             'www',
