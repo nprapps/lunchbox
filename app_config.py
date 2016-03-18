5c7ad1ef29c2bf0bcdcbbedc6eb28499c39801a8
Tyler Fisher <tyler@tylerjfisher.com>
2016-03-18 10:52:15 -0400

    moar updates
    Former-commit-id: ba46d5cd4198cf77613e497bcfc82658ef06209b

--- app_config.py
+++ app_config.py
@@ -26,7 +26,7 @@ GITHUB_USERNAME = 'nprapps'
 REPOSITORY_URL = 'git@github.com:%s/%s.git' % (GITHUB_USERNAME, REPOSITORY_NAME)
 REPOSITORY_ALT_URL = None # 'git@bitbucket.org:nprapps/%s.git' % REPOSITORY_NAME'
 
-DEV_CONTACT = 'EDIT THIS IN APP_CONFIG.PY'
+DEV_CONTACT = 'visuals@npr.org'
 
 """
 DEPLOYMENT
