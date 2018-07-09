d25336f83ded2724e5e7bc52de5e2f563465dc0c
Vanessa Qian <vvqian@berkeley.edu>
2018-07-09 15:03:03 -0400

    fixes chrome update caused bug

--- www/js/lib/dom-to-image.js
+++ www/js/lib/dom-to-image.js
@@ -207,6 +207,8 @@
                     if (source.cssText) target.cssText = source.cssText;
                     else copyProperties(source, target);
 
+                    target.fontStretch = 'normal'; // fixes chrome update bug
+
                     function copyProperties(source, target) {
                         util.asArray(source).forEach(function (name) {
                             target.setProperty(
