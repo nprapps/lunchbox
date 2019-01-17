91beb14ef5c23e4b3345a6999ba535eb827967c5
alykat <ahurt@npr.org>
2019-01-17 00:32:27 -0500

    reorder elements

--- www/js/annotator.js
+++ www/js/annotator.js
@@ -40,6 +40,7 @@ var render = function(state) {
       <blockquote class="quote" style="font-size: ${state.quoteSize}px">
         <p>${quoteParagraphs}</p>
       </blockquote>
+      <p class="show-credit">&mdash;&nbsp;${state.source}</p>
       <div class="annotation-wrapper">
           <div class="annotation" style="font-size: ${state.annotationSize}px">
               <p>${annotationParagraphs}</p>
@@ -49,7 +50,6 @@ var render = function(state) {
           </div>
           <div class="logo-wrapper"></div>
       </div>
-      <p class="show-credit">${state.source}</p>
   </div>`;
   preview.innerHTML = html;
 };
@@ -63,10 +63,10 @@ $.one("#save").addEventListener("click", async function(e) {
   //check heights
   var annotation = $.one(".poster .annotation-wrapper");
   var annoBounds = annotation.getBoundingClientRect();
-  var source = $.one(".show-credit");
-  var sourceBounds = source.getBoundingClientRect();
-  if (annoBounds.bottom >= sourceBounds.top) {
-    return alert("Your text seems to be too large for the social image size--reduce the font size or the text contents");
+  var poster = $.one(".poster");
+  var posterBounds = poster.getBoundingClientRect();
+  if (annoBounds.bottom >= posterBounds.bottom) {
+    return alert("Your text seems to be too large for the social image size. Reduce the font size or edit down the text.");
   }
 
   var image = await domtoimage.toPng(poster);
